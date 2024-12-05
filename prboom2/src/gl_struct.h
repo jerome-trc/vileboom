f5ef4a1ad3f7c008b50fd11ada0505f23e7f940f
Arsinikk <me@andrikpowell.com>
2024-12-04 18:07:57 -0600

    Fix missed gl_struct definition

--- prboom2/src/gl_struct.h
+++ prboom2/src/gl_struct.h
@@ -94,7 +94,7 @@ void gld_DrawLine(int x0, int y0, int x1, int y1, int BaseColor);
 void gld_DrawLine_f(float x0, float y0, float x1, float y1, int BaseColor);
 void gld_DrawWeapon(int weaponlump, vissprite_t *vis, int lightlevel);
 void gld_FillBlock(int x, int y, int width, int height, int col);
-void gld_DrawShaded(int x, int y, int width, int height, int col, dboolean animate);
+void gld_DrawShaded(int x, int y, int width, int height, int col, int screenshade);
 void gld_SetPalette(int palette);
 unsigned char *gld_ReadScreen(void);
 
