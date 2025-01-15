8d0e9682bcd5299fe8559c8992b0025b0993fb14
Arsinikk <me@andrikpowell.com>
2025-01-15 03:56:12 -0600

    Add consistency to `opengl-overlay-fix` branch

--- prboom2/src/gl_main.c
+++ prboom2/src/gl_main.c
@@ -852,8 +852,12 @@ void gld_FillBlock(int x, int y, int width, int height, int col)
 
 void gld_DrawShaded(int x, int y, int width, int height, int shade)
 {
-  color_rgb_t color = gld_LookupIndexedColor((invul_cm && !V_IsAutomapLightmodeIndexed()) ? playpal_white : playpal_black,
-                                             V_IsUILightmodeIndexed() || V_IsAutomapLightmodeIndexed());
+  // This is more messy than I'd like. (I hate this, but this is my current fix for the menu overlay invert)
+  // The `col` fixes the menu overlay from inverting during `invul_cm`.
+  // The 'automap` boolean is to undo the `col` invert for the automap.
+  dboolean automap = V_IsAutomapLightmodeIndexed();
+  int col = invul_cm && !automap ? playpal_white : playpal_black;
+  color_rgb_t color = gld_LookupIndexedColor(col, V_IsUILightmodeIndexed() || V_IsAutomapLightmodeIndexed());
 
   glsl_PushNullShader();
 
