e02c2be674cba8f7529330451110339a1514976c
Arsinikk <me@andrikpowell.com>
2025-01-25 03:22:20 -0600

    OpenGL Dark Overlay For Custom Invuln Palettes (Take 2)

--- prboom2/src/gl_main.c
+++ prboom2/src/gl_main.c
@@ -852,8 +852,12 @@ void gld_FillBlock(int x, int y, int width, int height, int col)
 
 void gld_DrawShaded(int x, int y, int width, int height, int shade)
 {
-  int black = 0;
-  color_rgb_t color = gld_LookupIndexedColor(black, V_IsUILightmodeIndexed() || V_IsAutomapLightmodeIndexed());
+  // This is more messy than I'd like. (I hate this, but this is my current fix for the menu overlay invert)
+  // The `col` fixes the menu overlay from inverting during `invul_cm`.
+  // The 'automap` boolean is to undo the `col` invert for the automap.
+  dboolean automap = V_IsAutomapLightmodeIndexed();
+  int col = invul_cm && !automap ? 256 : 0;
+  color_rgb_t color = gld_LookupIndexedColor(col, V_IsUILightmodeIndexed() || V_IsAutomapLightmodeIndexed());
 
   glsl_PushNullShader();
 
