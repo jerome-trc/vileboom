425f5c9fb03cf27c4a62320a9913900772e651f6
Arsinikk <me@andrikpowell.com>
2025-02-18 23:11:04 -0600

    Add custom message for multiple area map pickups

--- prboom2/src/p_inter.c
+++ prboom2/src/p_inter.c
@@ -667,7 +667,19 @@ void P_TouchSpecialThing(mobj_t *special, mobj_t *toucher)
       break;
 
     case SPR_PMAP:
-      if (!P_GivePower (player, pw_allmap) && !allow_incompatibility) // Pickup multiple area maps
+      if (player->powers[pw_allmap] && allow_incompatibility) // Pickup multiple area maps
+      {
+        dsda_string_t areamap_messsage;
+        dsda_StringPrintF(&areamap_messsage, "Another %s", s_GOTMAP);
+
+        doom_printf(areamap_messsage.string, player);
+        sound = sfx_getpow;
+
+        dsda_FreeString(&areamap_messsage);
+        break;
+      }
+
+      if (!P_GivePower (player, pw_allmap))
         return;
       dsda_AddPlayerMessage(s_GOTMAP, player);
       sound = sfx_getpow;
