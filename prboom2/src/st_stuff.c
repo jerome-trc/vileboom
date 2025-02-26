90e5430dec1e687ff381d52786dd6d6c70f903e0
Arsinikk <me@andrikpowell.com>
2025-02-26 13:42:57 -0600

    Tweak animate/wide stbar width code

--- prboom2/src/st_stuff.c
+++ prboom2/src/st_stuff.c
@@ -343,8 +343,6 @@ static patchnum_t faceback; // CPhipps - single background, translated for diffe
 
 //e6y: status bar background
 patchnum_t stbarbg;
-patchnum_t stbarbg_ws;
-patchnum_t stbarbg_ani;
 patchnum_t grnrock;
 patchnum_t brdr_t, brdr_b, brdr_l, brdr_r;
 patchnum_t brdr_tl, brdr_tr, brdr_bl, brdr_br;
@@ -469,9 +467,9 @@ void ST_SetScaledWidth(void)
   int width;
 
   if (Check_Stbar_Animate && animateLumps)
-      width = stbarbg_ani.width;
+      width = V_NamePatchWidth("S_STBAR");
   else if (Check_Stbar_Wide && widescreenLumps)
-      width = stbarbg_ws.width;
+      width = V_NamePatchWidth("W_STBAR");
   else if (stbar_exists)
       width = stbarbg.width;
   else
@@ -1267,10 +1265,6 @@ static void ST_loadGraphics(void)
   stbar_exists = W_LumpNameExists(stbar);
 
   //e6y: status bar background
-  if (Check_Stbar_Animate)
-    R_SetPatchNum(&stbarbg_ani, "S_STBAR");
-  if (Check_Stbar_Wide)
-    R_SetPatchNum(&stbarbg_ws, "W_STBAR");
   if (stbar_exists)
     R_SetPatchNum(&stbarbg, stbar);
   R_SetPatchNum(&brdr_t, "brdr_t");
